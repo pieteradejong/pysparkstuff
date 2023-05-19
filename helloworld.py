b6277379e0cd269f3063c2fbf89c425dd9d06dc3
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-05-19 18:19:30 -0400

    put log levels in enum

--- helloworld.py
+++ helloworld.py
@@ -1,17 +1,24 @@
+from enum import Enum
 from pyspark.sql import SparkSession
 
-SPARK_LOG_LEVELS = {
-
-    # ALL, DEBUG, ERROR, FATAL, INFO, OFF, TRACE, WARN
-}
+class LOG_LEVELS(Enum):
+    ALL   = 'ALL'
+    DEBUG = 'DEBUG'
+    ERROR = 'ERROR'
+    FATAL = 'FATAL'
+    INFO  = 'INFO'
+    OFF   = 'OFF'
+    TRACE = 'TRACE'
+    WARN  = 'WARN'
 
 print("Hello, PySpark!")
+
 spark = SparkSession.builder.appName("Hello PySpark") \
     .config("spark.memory.offHeap.enabled", "true") \
     .config("spark.memory.offHeap.size", "10g").getOrCreate()
 
 sc = spark.sparkContext
-sc.setLogLevel("ERROR")
+sc.setLogLevel(LOG_LEVELS.ERROR)
 
 df = spark.read.csv("Online Retail.csv", header=True, escape="\"")
 # df = spark.read.csv("test.csv", header=True, escape="\"")
